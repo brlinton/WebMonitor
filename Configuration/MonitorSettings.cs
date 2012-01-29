a05cbab9ac11b5e9df2bdab8920c0e0751738d11
Brandon Linton <linton.accounts@gmail.com>
2012-01-29 15:18:21 -0500

    Very initial set of functionality - monitor one website for a given status code (200) on execution

---
+++ Configuration/MonitorSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Net;
+
+namespace WebMonitor.Configuration
+{
+    // http://haacked.com/archive/2007/03/11/custom-configuration-sections-in-3-easy-steps.aspx
+    public class MonitorSettings : ConfigurationSection
+    {
+        private static MonitorSettings settings = ConfigurationManager.GetSection("MonitorSettings") as MonitorSettings;
+
+        public static MonitorSettings Settings
+        {
+            get
+            {
+                return settings;
+            }
+        }
+
+        [ConfigurationProperty("uri", IsRequired = true)]
+        public string Uri
+        {
+            get { return (string)this["uri"]; }
+            set { this["uri"] = value; }
+        }
+
+        [ConfigurationProperty("httpStatusCode", IsRequired = false)]
+        public HttpStatusCode HttpStatusCode
+        {
+            get 
+            {
+                var code = (HttpStatusCode)this["httpStatusCode"];
+                
+                if (code == 0)
+                {
+                    code = HttpStatusCode.OK;
+                }
+
+                return code; 
+            }
+            set 
+            { 
+                this["httpStatusCode"] = value; 
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
+using WebMonitor.Configuration;
 
 namespace WebMonitor
 {
@@ -9,9 +9,50 @@ namespace WebMonitor
     {
         static void Main(string[] args)
         {
+            var config = MonitorSettings.Settings;
+            var isHealthy = IsHealthy(config);
+
+            Console.WriteLine("Is {0} healthy? {1}", config.Uri, isHealthy);
             // TODO - read configuration, building up things to do
             // TODO - spin through the things to do
             // TODO - on failure of the check, execute its settings for how to notify
+        }
+
+        public static bool IsHealthy(MonitorSettings config)
+        {
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create(config.Uri);
+            
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            
+            // Display the status.
+            //Console.WriteLine(response.StatusDescription);
+            
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            
+            // Display the content.
+            //  TODO - should we display a snippet of the response
+            //Console.WriteLine(responseFromServer);
+            
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            var isHealthy = config.HttpStatusCode == response.StatusCode;
+
+            return isHealthy;
         }
     }
 }
